1fed3d3960d5b197d944b8e262454f0b0a380cf0
David Dugle <david.dugleii@yahoo.com>
2021-02-09 17:11:14 -0500

    functional components

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -1,22 +1,14 @@
 import React from "react";
 
 export default class AddOption extends React.Component {
-    constructor(props) {
-      super(props);
-      this.handleAddOption = this.handleAddOption.bind(this);
-      this.state = {
-        error: undefined
-      };
-    }
-    handleAddOption(e) {
+
+  state = {
+    error: undefined
+  }
+    handleAddOption = (e) => {
       e.preventDefault();
-  
       const option = e.target.elements.option.value.trim();
       const error = this.props.handleAddOption(option);
-  
-    //   this.setState(() => {
-    //     return { error };
-    //   });
 
       this.setState(() => ({error}))
 
@@ -24,7 +16,7 @@ export default class AddOption extends React.Component {
             e.target.elements.option.value = "";
         }
 
-    }
+    };
     render() {
       return (
         <div>
