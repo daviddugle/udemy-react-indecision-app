767b68b1450cc211ca8c64af0efc30b1687112b7
David Dugle <david.dugleii@yahoo.com>
2021-02-10 09:15:51 -0500

    media queries

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -8,12 +8,13 @@ const OptionModal = (props) => (
     onRequestClose={props.handleCloseModal}
     contentLabel="Selected Option"
     closeTimeoutMS={200}
+    className="modal"
     ariaHideApp={false}
     >
     
-    <h3>Selected Option</h3>
-    {props.selectedOption && <p>{props.selectedOption}</p>}
-    <button onClick={props.handleCloseModal}>Let's Do It!</button>
+    <h3 className="modal__title">Selected Option</h3>
+    {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+    <button className="button" onClick={props.handleCloseModal}>Let's Do It!</button>
     
     </Modal>
     
